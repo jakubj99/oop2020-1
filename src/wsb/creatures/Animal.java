5e2614ca9c411ec59a0202b9b91ebf0d100ddeec
jakubjankowski99 <jakubj31@gmail.com>
2021-03-20 07:50:34 +0100

    Task 3&4

--- src/wsb/creatures/Animal.java
+++ src/wsb/creatures/Animal.java
@@ -6,21 +6,22 @@ import java.io.File;
 import java.sql.SQLException;
 
 public class Animal implements Feedable, Comparable<Animal> {
-    final String species;
-    private Double weight;
-    public String name;
-    File pic;
-
 
     private static Double NEW_DOG_WEIGHT = 4.0;
     private static Double NEW_LION_WEIGHT = 39.2;
     private static Double NEW_OTHER_ANIMAL_WEIGHT = 5.3;
-
     private static Double DEFAULT_FEED_WEIGHT = 1.0;
 
-    public Animal(String species) {
+    final String species;
+    private Double weight;
+    public String name;
+    private final FoodType foodType;
+    File pic;
+
+    public Animal(String species, FoodType foodType) {
         System.out.println("we created new Animal");
         this.species = species;
+        this.foodType = foodType;
 
         switch (species) {
             case "dog": {
@@ -42,17 +43,6 @@ public class Animal implements Feedable, Comparable<Animal> {
             e.printStackTrace();
         }
     }
-
-    public Animal(String species, Double weight) {
-        this.weight = weight;
-        this.species = species;
-        try {
-            this.save();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     public void feed() {
         feed(DEFAULT_FEED_WEIGHT);
@@ -105,4 +95,26 @@ public class Animal implements Feedable, Comparable<Animal> {
         System.out.println(sql);
 //        Connector.executeSQL(sql);
     }
+
+    private void eat(Double foodWeight) {
+        switch (foodType) {
+            case MEAT:
+                digest(foodWeight, .7);
+                break;
+            case CROPS:
+                digest(foodWeight, .3d);
+                break;
+            case ALL:
+                digest(foodWeight, .5d);
+                break;
+            default:
+                digest(-foodWeight, .5d);
+                break;
+        }
+    }
+
+    private void digest(Double foodWeight, Double multiplier) {
+        weight += foodWeight * multiplier;
+    }
+
 }
