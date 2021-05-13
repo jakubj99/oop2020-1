ced784c4724af4549fa2b4a2e9796e3a48b71306
jakubjankowski99 <jakubj31@gmail.com>
2021-05-13 18:49:25 +0200

    Task 8

--- src/wsb/Main.java
+++ src/wsb/Main.java
@@ -1,55 +1,32 @@
 package wsb;
 
-import wsb.devices.*;
+import wsb.creatures.Animal;
+import wsb.creatures.FarmAnimal;
+import wsb.creatures.Human;
+import wsb.creatures.Pet;
+import wsb.creatures.enums.Gender;
 
+import java.sql.SQLException;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-
-        Car ford = new DieselCar("VW", "Polo", 2019, 2.0);
-        ford.startACar();
-        ford.stopACar();
-
-
-        Map<String, Double> countryAreaMap = Stream.of(
-            new AbstractMap.SimpleEntry<>("Czech Republic", 75d),
-            new AbstractMap.SimpleEntry<>("Germany", 100d),
-            new AbstractMap.SimpleEntry<>("Norway", 40d),
-            new AbstractMap.SimpleEntry<>("Sweden", 1d),
-            new AbstractMap.SimpleEntry<>("Kaczoland", 10d))
-            .collect(Collectors.toMap(
-                    AbstractMap.SimpleEntry::getKey,
-                    AbstractMap.SimpleEntry::getValue));
-
-         System.out.println("Largest country - " + countryAreaMap.entrySet().stream()
-                 .max(Map.Entry.comparingByValue())
-            .map(Map.Entry::getKey)
-                 .orElse(""));
-
-
-
-         System.out.println("Smallest country - " + countryAreaMap.entrySet().stream()
-                 .min(Map.Entry.comparingByValue())
-            .map(Map.Entry::getKey)
-                 .orElse(""));
-
-
-        List<Device> devices = Arrays.asList(
-                Phone.createIPhone("7", 4.7),
-                Phone.createIPhone("12 pro", 6.1),
-                Phone.createTrashPhone("Samsung", "S10", 6.9),
-                Phone.createTrashPhone("Samsung", "S20", 7.2)
+    public static void main(String[] args) throws SQLException {
+        List<Animal> animals = Arrays.asList(
+                new Human(13, 20, Gender.MAN, 55),
+                new Pet("York", 0.9),
+                new FarmAnimal("George", 120),
+                new Human(69, 0, Gender.WOMAN, 46),
+                new FarmAnimal("Peppa", 200)
         );
-
-        Map<String, List<Device>> producerDevicesMap = devices.stream().collect(
-                Collectors.groupingBy(Device::getProducer, HashMap::new, Collectors.toCollection(ArrayList::new)));
-
-        System.out.println("Devices produced by Apple :" + producerDevicesMap.get("Apple"));
-        System.out.println("Devices produced by Samsung :" + producerDevicesMap.get("Samsung"));
-
+        animals.sort(new Comparator<Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                return Double.compare(o1.getWeight(), o2.getWeight());
+            }
+        });
+        System.out.println(animals);
     }
 }
+
+
 
