e7484e816995528b50e58161b4e2a1d5c770d92f
jakubjankowski99 <jakubj31@gmail.com>
2021-05-13 19:22:17 +0200

    Task 7.1

--- src/wsb/Main.java
+++ src/wsb/Main.java
@@ -1,53 +1,12 @@
 package wsb;
 
-import wsb.devices.Device;
-import wsb.devices.DieselCar;
-import wsb.devices.ElectricCar;
-import wsb.devices.Phone;
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import wsb.devices.*;
 
 public class Main {
     public static void main(String[] args) {
-
-    Map<String, Double> countryAreaMap = Stream.of(
-            new AbstractMap.SimpleEntry<>("Czech Republic", 75d),
-            new AbstractMap.SimpleEntry<>("Germany", 100d),
-            new AbstractMap.SimpleEntry<>("Norway", 40d),
-            new AbstractMap.SimpleEntry<>("Sweden", 1d),
-            new AbstractMap.SimpleEntry<>("Kaczoland", 10d))
-            .collect(Collectors.toMap(
-                    AbstractMap.SimpleEntry::getKey,
-                    AbstractMap.SimpleEntry::getValue));
-
-         System.out.println("Largest country - " + countryAreaMap.entrySet().stream()
-                 .max(Map.Entry.comparingByValue())
-            .map(Map.Entry::getKey)
-                 .orElse(""));
-
-
-
-         System.out.println("Smallest country - " + countryAreaMap.entrySet().stream()
-                 .min(Map.Entry.comparingByValue())
-            .map(Map.Entry::getKey)
-                 .orElse(""));
-
-
-        List<Device> devices = Arrays.asList(
-                Phone.createIPhone("7", 4.7),
-                Phone.createIPhone("12 pro", 6.1),
-                Phone.createTrashPhone("Samsung", "S10", 6.9),
-                Phone.createTrashPhone("Samsung", "S20", 7.2)
-        );
-
-        Map<String, List<Device>> producerDevicesMap = devices.stream().collect(
-                Collectors.groupingBy(Device::getProducer, HashMap::new, Collectors.toCollection(ArrayList::new)));
-
-        System.out.println("Devices produced by Apple :" + producerDevicesMap.get("Apple"));
-        System.out.println("Devices produced by Samsung :" + producerDevicesMap.get("Samsung"));
-
+        Car car = new DieselCar("VW", "Polo", 2019, 2.0);
+        car.startACar();
+        car.stopACar();
     }
 }
 
--- src/wsb/devices/Car.java
+++ src/wsb/devices/Car.java
@@ -1,5 +1,4 @@
 package wsb.devices;
-
 import wsb.creatures.Human;
 import wsb.Soldable;
 
@@ -9,19 +8,30 @@ public abstract class Car extends Device implements Soldable, Comparable<Car> {
     public final Integer yearOfProduction;
     public final Double sizeOfAnEngine;
     public String plates;
+    private Engine engine;
 
     public Car(String producer, String model, Integer yearOfProduction, Double sizeOfAnEngine) {
         super(producer, model);
         this.yearOfProduction = yearOfProduction;
         this.sizeOfAnEngine = sizeOfAnEngine;
 
+        this.engine = new Engine(150d, sizeOfAnEngine, 0d);
     }
 
     abstract public void refuel();
 
     @Override
     public void turnOn() {
-        System.out.println("car is ready to go");
+        System.out.println("car is switched on");
+        startACar();
+    }
+
+    public void stopACar() {
+        engine.turnOff();
+    }
+
+    public void startACar() {
+        engine.turnOn();
     }
 
     public String toString() {
@@ -39,17 +49,41 @@ public abstract class Car extends Device implements Soldable, Comparable<Car> {
             throw new Exception("seller don't have a car");
         }
         if(!buyer.hasAFreePlace()){
-            throw new Exception("bouer dont have a plase");
+
+            throw new Exception("buyer dont have a car");
         }
         if(buyer.cash < price){
-            throw new Exception("afwfwaawfawf");
+            throw new Exception("exception");
         }
         buyer.removeCar(this);
         seller.addCar(this);
         buyer.cash -= price;
         seller.cash += price;
-        System.out.println("great, transaction is done");
+        System.out.println("great success");
     }
 
+    private static class Engine {
+
+        private boolean isOn;
+        private Double horsePower;
+        private Double millage;
+        private Double volume;
+
+        public Engine(Double horsePower, Double millage, Double volume) {
+            this.horsePower = horsePower;
+            this.millage = millage;
+            this.volume = volume;
+        }
+
+        public void turnOn() {
+            isOn = true;
+            System.out.println("engine starts");
+        }
+
+        public void turnOff() {
+            isOn = false;
+            System.out.println("engine stops");
+        }
+    }
 
 }
