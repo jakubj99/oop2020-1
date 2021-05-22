0ca55cd2949b469ead70781894e244d527e7ee7e
jakubjankowski99 <jakubj31@gmail.com>
2021-05-22 18:58:47 +0200

    Task 12

--- src/wsb/Main.java
+++ src/wsb/Main.java
@@ -1,26 +1,14 @@
 package wsb;
 
-import wsb.creatures.Animal;
-import wsb.creatures.FarmAnimal;
-import wsb.creatures.Human;
-import wsb.creatures.Pet;
-import wsb.creatures.enums.Gender;
-
-import java.sql.SQLException;
-import java.util.*;
+import wsb.database.ObjectToSQL;
+import wsb.devices.Car;
+import wsb.devices.ElectricCar;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-
-        List<Animal> animals = Arrays.asList(
-                new Human(13, 20, Gender.MAN, 55),
-                new Pet("York", 0.9),
-                new FarmAnimal("George", 120),
-                new Human(69, 0, Gender.WOMAN, 46),
-                new FarmAnimal("Peppa", 200));
-
-        animals.sort(Comparator.comparingDouble(Animal::getWeight));
-        System.out.println(animals);
+    public static void main(String[] args) {
+        Car car = new ElectricCar("Tesla", "Model S", 2019);
+        ObjectToSQL objectToSql = new ObjectToSQL();
+        System.out.println(objectToSql.insert(car));
     }
 }
 
