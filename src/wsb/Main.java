93fd0267c8fde4a558a3366067b1b5b731bfd642
jakubjankowski99 <jakubj31@gmail.com>
2021-05-22 18:43:30 +0200

    Task 11

--- src/wsb/Main.java
+++ src/wsb/Main.java
@@ -1,52 +1,27 @@
 package wsb;
 
-import wsb.threads.CallableSorter;
+import wsb.creatures.Animal;
+import wsb.creatures.FarmAnimal;
+import wsb.creatures.Human;
+import wsb.creatures.Pet;
+import wsb.creatures.enums.Gender;
 
+import java.sql.SQLException;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException{
-        List<Integer> list1 = generateList(10);
-        List<Integer> list2 = generateList(100);
-        List<Integer> list3 = generateList(1000);
-        List<Integer> list4 = generateList(10000);
+    public static void main(String[] args) throws SQLException {
 
-        ExecutorService singleExecutor = Executors.newSingleThreadExecutor();
-        Date start = new Date();
-        Collection<Future<List<Integer>>> futureNumbers = singleExecutor.invokeAll(Arrays.asList(
-                new CallableSorter(list1),
-                new CallableSorter(list2),
-                new CallableSorter(list3),
-                new CallableSorter(list4)));
+        List<Animal> animals = Arrays.asList(
+                new Human(13, 20, Gender.MAN, 55),
+                new Pet("York", 0.9),
+                new FarmAnimal("George", 120),
+                new Human(69, 0, Gender.WOMAN, 46),
+                new FarmAnimal("Peppa", 200));
 
-        ExecutorService multiExecutor = Executors.newFixedThreadPool(4);
-        Date start2 = new Date();
-        Collection<Future<List<Integer>>> futureNumbersSortedAtSametime = multiExecutor.invokeAll(Arrays.asList(
-                new CallableSorter(list1),
-                new CallableSorter(list2),
-                new CallableSorter(list3),
-                new CallableSorter(list4)
-        ));
-
-        singleExecutor.shutdown();
-        System.out.println("Single thread sorting (ms): " + (new Date().getTime() - start.getTime()));
-        multiExecutor.shutdown();
-        System.out.println("Multi thread sorting (ms): " + (new Date().getTime() - start2.getTime()));
-
-    }
-
-    private static List<Integer> generateList(int size) {
-        Random random = new Random();
-        Set<Integer> set = new HashSet<>();
-        while (set.size() != size) {
-            set.add(random.nextInt(Integer.MAX_VALUE));
-        }
-        return new ArrayList<>(set);
+        animals.sort(Comparator.comparingDouble(Animal::getWeight));
+        System.out.println(animals);
     }
 }
-
 
 
